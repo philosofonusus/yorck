c5e2bc2a61eaf8f3aa90f48b47c334807e7a7b14
TENTACLE <my.tentacles.are.in.you@gmail.com>
2023-07-09 22:30:51 +0200

    memo

--- src/app/lists/[id]/Tabs/PortfolioTab.tsx
+++ src/app/lists/[id]/Tabs/PortfolioTab.tsx
@@ -13,6 +13,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { useMemo } from "react";
 
 const formatter = new Intl.NumberFormat("en-US", {
   style: "currency",
@@ -23,63 +24,68 @@ const PortfolioTab: React.FC = () => {
   const [listInfo, setListInfo] = useAtom(listInfoAtom);
   const [value, copy] = useCopyToClipboard();
 
+  const totalPortfolio = useMemo(
+    () =>
+      listInfo.selectedRows
+        .map((el: any) => el?.balances)
+        .filter(Boolean)
+        .flat()
+        .filter((el: any) => el.price * el.amount > 500)
+        .sort((a: any, b: any) => b.price * b.amount - a.price * a.amount)
+        .reduce(function (accumulator: any, cur: any) {
+          const id = cur.id,
+            found = accumulator.find(function (el: any) {
+              return el.id == id;
+            });
+          if (found) found.amount += cur.amount;
+          else accumulator.push(cur);
+          return accumulator;
+        }, []),
+    [listInfo.selectedRows]
+  );
+
   return listInfo?.selectedRows?.length ? (
     <TabsContent value="portfolio">
       <Card className="p-6 flex flex-wrap gap-4">
-        {listInfo.selectedRows
-          .map((el: any) => el?.balances)
-          .filter(Boolean)
-          .flat()
-          .filter((el: any) => el.price * el.amount > 500)
-          .sort((a: any, b: any) => b.price * b.amount - a.price * a.amount)
-          .reduce(function (accumulator: any, cur: any) {
-            const id = cur.id,
-              found = accumulator.find(function (el: any) {
-                return el.id == id;
-              });
-            if (found) found.amount += cur.amount;
-            else accumulator.push(cur);
-            return accumulator;
-          }, [])
-          .map((el: any, idx: number) => {
-            return (
-              <div key={idx} className="flex items-center gap-2">
-                {el.logo_url ? (
-                  <Image
-                    alt={el.symbol}
-                    src={el.logo_url}
-                    width={40}
-                    height={40}
-                    className="h-10 w-10 rounded-full"
-                  />
-                ) : (
-                  <div className="h-10 w-10 rounded-full bg-accent" />
-                )}
-                <div className="flex flex-col gap-1">
-                  <TooltipProvider>
-                    <Tooltip>
-                      <TooltipTrigger className="text-left">
-                        <span
-                          onClick={() => {
-                            copy(el.id);
-                            toast.success("Copied to clipboard");
-                          }}
-                          className="cursor-pointer text-sm font-semibold text-muted-foreground"
-                        >
-                          {el.optimized_symbol} ({el.chain})
-                        </span>
-                      </TooltipTrigger>
-                      <TooltipContent>{el.id}</TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
+        {totalPortfolio.map((el: any, idx: number) => {
+          return (
+            <div key={idx} className="flex items-center gap-2">
+              {el.logo_url ? (
+                <Image
+                  alt={el.symbol}
+                  src={el.logo_url}
+                  width={40}
+                  height={40}
+                  className="h-10 w-10 rounded-full"
+                />
+              ) : (
+                <div className="h-10 w-10 rounded-full bg-accent" />
+              )}
+              <div className="flex flex-col gap-1">
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger className="text-left">
+                      <span
+                        onClick={() => {
+                          copy(el.id);
+                          toast.success("Copied to clipboard");
+                        }}
+                        className="cursor-pointer text-sm font-semibold text-muted-foreground"
+                      >
+                        {el.optimized_symbol} ({el.chain})
+                      </span>
+                    </TooltipTrigger>
+                    <TooltipContent>{el.id}</TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
 
-                  <span className="text-sm font-semibold">
-                    {formatter.format(el.price * el.amount)}
-                  </span>
-                </div>
+                <span className="text-sm font-semibold">
+                  {formatter.format(el.price * el.amount)}
+                </span>
               </div>
-            );
-          })}
+            </div>
+          );
+        })}
       </Card>
     </TabsContent>
   ) : null;
