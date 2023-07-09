958988d5e5cdee0a486dc1e8865d0f27010606db
TENTACLE <32138582+philosofonusus@users.noreply.github.com>
2023-07-09 19:06:37 +0000

    restyle email address

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -37,9 +37,6 @@ export default function Header() {
             <DropdownMenuLabel className="font-normal">
               <div className="flex flex-col space-y-1">
                 <p className="text-sm font-medium leading-none">
-                  {user?.username}
-                </p>
-                <p className="text-xs leading-none text-muted-foreground">
                   {user?.primaryEmailAddress?.emailAddress}
                 </p>
               </div>
