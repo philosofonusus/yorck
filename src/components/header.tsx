39ed8f020487d332a3dfab430292be466decfe0f
TENTACLE <32138582+philosofonusus@users.noreply.github.com>
2023-07-09 19:17:35 +0000

    header styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -36,7 +36,7 @@ export default function Header() {
           <DropdownMenuContent className="w-56" align="end" forceMount>
             <DropdownMenuLabel className="font-normal">
               <div className="flex flex-col space-y-1">
-                <p className="text-sm font-medium leading-none">
+                <p className="text-xs leading-none text-muted-foreground">
                   {user?.primaryEmailAddress?.emailAddress}
                 </p>
               </div>
