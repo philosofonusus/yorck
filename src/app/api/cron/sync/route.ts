a1c318f58bc03bf025a8aa3d3152a8749d27ede7
TENTACLES <my.tentacles.are.in.you@gmail.com>
2024-02-18 17:46:52 +0100

    hotfix

--- src/app/api/cron/sync/route.ts
+++ src/app/api/cron/sync/route.ts
@@ -8,6 +8,7 @@ export async function GET() {
   const lists = await db.select().from(addressLists);
 
   const addresses = lists.map((list) => list.addresses as string[]).flat();
-  await monitofresh.refreshAddressData(addresses, env.API_TOKEN);
+  monitofresh.refreshAddressData(addresses, env.API_TOKEN);
+
   return NextResponse.json({ ok: true });
 }
